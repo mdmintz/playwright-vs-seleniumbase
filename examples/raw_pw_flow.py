0cd50c7ff67131300dac18e51617186547548cab
Michael Mintz <mdmintz@gmail.com>
2025-01-28 13:44:05 -0500

    Update examples

--- examples/raw_pw_flow.py
+++ examples/raw_pw_flow.py
@@ -4,20 +4,13 @@ from seleniumbase import decorators
 
 @decorators.print_runtime("Playwright flow, test-only")
 def run(page):
-    page.goto("https://www.saucedemo.com")
-    page.locator("#user-name")
-    page.fill("#user-name", "standard_user")
-    page.fill("#password", "secret_sauce")
-    page.locator('input[type="submit"]').click()
-    page.locator('button[name*="backpack"]').click()
-    page.locator("#shopping_cart_container a").click()
-    page.locator("button#checkout").click()
-    page.fill("input#first-name", "SeleniumBase")
-    page.fill("input#last-name", "Automation")
-    page.fill("input#postal-code", "77123")
-    page.locator("input#continue").click()
-    page.locator("button#finish").click()
-    page.locator('img[alt="Pony Express"]')
+    page.goto("https://seleniumbase.github.io/coffee/")
+    page.locator('div[data-sb="Mocha"]').click()
+    page.locator("button.pay").click()
+    page.fill("input#name", "Selenium Coffee")
+    page.fill("input#email", "test@test.test")
+    page.locator("button#submit-payment").click()
+    page.locator("#app div.success")
     print(page.url)
 
 
--- examples/raw_sb_flow.py
+++ examples/raw_sb_flow.py
@@ -4,25 +4,19 @@ from seleniumbase import decorators
 
 @decorators.print_runtime("SeleniumBase flow, test-only")
 def run(sb):
-    sb.open("https://www.saucedemo.com")
-    sb.send_keys("#user-name", "standard_user")
-    sb.send_keys("#password", "secret_sauce")
-    sb.click('input[type="submit"]')
-    sb.click('button[name*="backpack"]')
-    sb.click("#shopping_cart_container a")
-    sb.click("button#checkout")
-    sb.send_keys("input#first-name", "SeleniumBase")
-    sb.send_keys("input#last-name", "Automation")
-    sb.send_keys("input#postal-code", "77123")
-    sb.click("input#continue")
-    sb.click("button#finish")
-    sb.assert_element('img[alt="Pony Express"]')
-    print(sb.get_current_url())
+    sb.driver.get("https://seleniumbase.github.io/coffee/")
+    sb.driver.click('div[data-sb="Mocha"]')
+    sb.driver.click("button.pay")
+    sb.driver.send_keys("input#name", "Selenium Coffee")
+    sb.driver.send_keys("input#email", "test@test.test")
+    sb.driver.click("button#submit-payment")
+    sb.driver.find_element("#app div.success")
+    print(sb.driver.current_url)
 
 
 @decorators.print_runtime("SeleniumBase flow with launch")
 def main():
-    with SB() as sb:
+    with SB(pls="none") as sb:
         run(sb)
 
 
