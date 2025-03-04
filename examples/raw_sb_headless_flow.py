94a1240eeea72bdce1dbdcbd487eb45f7932b64b
Michael Mintz <mdmintz@gmail.com>
2025-03-04 17:17:48 -0500

    Update a script

--- examples/raw_sb_headless_flow.py
+++ examples/raw_sb_headless_flow.py
@@ -5,10 +5,8 @@ from seleniumbase import decorators
 @decorators.print_runtime("SeleniumBase flow, test-only")
 def run(sb):
     sb.driver.get("https://seleniumbase.github.io/coffee/")
-    sb.driver.wait_for_element("button.pay")
     sb.driver.click('div[data-sb="Mocha"]')
     sb.driver.click("button.pay")
-    sb.driver.wait_for_element("input#name")
     sb.driver.send_keys("input#name", "Selenium Coffee")
     sb.driver.send_keys("input#email", "test@test.test")
     sb.driver.click("button#submit-payment")
